f9b10a6da39696d2279394429c390206bc67a2ac
milkamt <mimamatuskova@gmail.com>
2022-12-02 20:13:46 +0100

    add url model & repository

---
+++ src/main/java/com/url_shortener/model/Url.java
@@ -0,0 +1,34 @@
+package com.url_shortener.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Url {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @URL
+    private String url;
+    @Size(max = 30)
+    private String alias;
+    private String secretCode;
+    private Long hitCount;
+
+    public Url(String url, String alias, String secretCode) {
+        this.url = url;
+        this.alias = alias;
+        this.secretCode = secretCode;
+    }
+}
---
+++ src/main/java/com/url_shortener/repository/UrlRepository.java
@@ -0,0 +1,9 @@
+package com.url_shortener.repository;
+
+import com.url_shortener.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, Long> {
+}
